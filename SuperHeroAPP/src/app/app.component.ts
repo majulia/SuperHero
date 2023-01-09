f7c7c25d9e9457420d4b44781eb6981fcf661d73
Maria Julia de Oliveira <52303035+majulia@users.noreply.github.com>
2023-01-09 12:10:17 -0300

    crud angular

--- SuperHeroAPP/src/app/app.component.ts
+++ SuperHeroAPP/src/app/app.component.ts
@@ -19,6 +19,10 @@ export class AppComponent {
       .subscribe((result: SuperHero[]) => (this.heroes = result));
   }
 
+  updateHeroList(heroes: SuperHero[]) {
+    this.heroes = heroes;
+  }
+
   initNewHero() {
     this.heroToEdit = new SuperHero();
   }
--- SuperHeroAPP/src/app/services/super-hero.service.ts
+++ SuperHeroAPP/src/app/services/super-hero.service.ts
@@ -14,4 +14,16 @@ export class SuperHeroService {
   public getSuperHeroes(): Observable<SuperHero[]> {
     return this.http.get<SuperHero[]>(`${environment.apiUrl}/${this.url}`);
   }
+
+  public createHero(hero: SuperHero): Observable<SuperHero[]> {
+    return this.http.post<SuperHero[]>(`${environment.apiUrl}/${this.url}`, hero);
+  }
+
+  public updateHero(hero: SuperHero): Observable<SuperHero[]> {
+    return this.http.put<SuperHero[]>(`${environment.apiUrl}/${this.url}`, hero);
+  }
+
+  public deleteHero(hero: SuperHero): Observable<SuperHero[]> {
+    return this.http.delete<SuperHero[]>(`${environment.apiUrl}/${this.url}/${hero.id}`);
+  }
 }
